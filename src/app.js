ea15885fb01c17f1c587a782e970f01a482e1e60
akis <flaflabell@gmail.com>
2022-02-02 22:42:45 +0900

    scssを変換→cssをバンドル→HTMLに注入を行う設定

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 // エントリーポイント
-import './sub'
+import './sub';
+import './app.scss';
 
 console.log('this is a main js file.');
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,21 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: {app: './src/app.js', sub: './src/sub.js'},
+  entry: {app: './src/app.js'},
   output:  {
     path: path.resolve(__dirname, 'public'), // 出力先の絶対パスを指定
     filename: '[name].bundle.js' // 出力ファイル名
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/, // 拡張子.scssのファイルを対象とする
+        use: [ // どのloaderを使うかを設定　下から順に実行される。
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
   }
 }
